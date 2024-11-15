a3f406ce080950fbdc8095fdd2209365c07c0dad
Carson Secrest <carsonsecrest@gmail.com>
2024-11-14 16:09:33 -0800

    Move React project into client-web folder

---
+++ client-web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex flex-row w-full h-full justify-center items-center bg-gray-400'>
+      <div className='flex flex-col'></div>
+    </div>
+  )
+}
+
+export default App
